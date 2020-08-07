b8455b03852357a3afb4ffe5ab44a09f0098d808
mztiai <miikka.tiainen@aalto.fi>
2020-08-07 15:35:03 +0300

    3: continuous case and estimators (both)

--- contSimul.R
+++ contSimul.R
@@ -1,1 +1,119 @@
 # FOR CONTINUOUS SIMUL CODE
+
+distr_a <- function(ta){
+  runif(1,1,2*ta-1)
+  #rexp(1,1/ta)
+}
+
+distr_b <- function(tb){
+  #runif(1,0,2*tb)
+  #runif(1,1.5,2*tb-1.5)
+  rexp(1,1/tb)
+  #rgeom(1,tb)
+}
+
+draw_ka <- function(t, ka, multiplier){
+  if(t>=5 && t<= 10){
+    return (multiplier*ka)
+  }else return (ka)
+}
+
+draw_kb <- function(t, kb, multiplier){
+  if(t>=5 && t<= 10){
+    return (multiplier*kb)
+  }else return (kb)
+}
+
+simulation_varying_time <- function(n_events, ta,ka,tb,kb,pa=0.5, S0,E0, seed = FALSE){
+  require("collections")
+  if(seed == TRUE){
+    set.seed(1234)     # for repeatability
+  }
+  times <- rep(0,n_events)
+  I0 <- 0
+  R0 <- 0
+  total_population <- S0+E0+I0+R0
+  
+  S <- append(S0,rep(0,n_events-1))
+  E <- append(E0,rep(0,n_events-1))
+  I <- append(I0,rep(0,n_events-1))
+  R <- append(R0,rep(0,n_events-1))
+  
+  Q <- priority_queue()
+  
+  # initial infectee(s) 
+  for(i in 1:E0){
+    if(runif(1) < pa){
+      ti <- distr_a(ta)        # draw ta from distribution
+      Q$push(c("a",ti),-ti)    # add status to PQ, prioritize small times -> negate priority
+      #print("added 1 a")
+    }else{
+      ti <- distr_b(tb)       # draw tb from distribution
+      Q$push(c("b",ti),-ti)   # add status to PQ
+      #print("added 1 b")
+    }
+  }
+  
+  # infection process based on intial state and queue
+  for(t in 2:n_events){
+    # initialize state as same as previous state
+    S[t]<-S[t] + S[t-1]
+    E[t]<-E[t] + E[t-1]
+    R[t]<-R[t] + R[t-1]
+    times[t] <- times[t] + times[t-1]
+    
+    # pop next infector from PQ - if empty no one will infect anymore and the process loops to the end
+    if(Q$size() != 0){
+      infected <- 0
+      
+      type_and_time <- Q$pop()
+      infector_type <- type_and_time[1]
+      infector_time <- as.numeric(type_and_time[2])
+      
+      # infect based on previously drawn (ti,ki) pair status of infector
+      # earliest entry of queue must be earliest infector
+      
+      # if infector of type a it infects according to ka:
+      if(infector_type == "a"){
+        #print("individual of type a - ka new infections")
+        infectivity <- min(1,ka/total_population)   # OLD - not needed???
+        infected <- rbinom(1,S[t], infectivity)
+        I[t] <- I[t] + 1
+        # else of type b and infect kb individuals:
+      }else if(infector_type == "b"){
+        #print("individual of type b - kb new infections")
+        infectivity <- min(1,kb/total_population)
+        infected <- rbinom(1,S[t], infectivity)
+        I[t] <- I[t] - 1 # allows reading total number of a or b infectors in the epidemic by +/-
+      }
+      
+      #print(c("infect ", infected, " at row ", t))
+      
+      # individual has now infected ka/kb people - update state:
+      S[t] <- S[t] - infected
+      E[t] <- E[t] + infected - 1 
+      # I[t]  set in if clause; a+=1, b-=1
+      R[t+1] <- R[t+1] + 1 
+      times[t] <- infector_time # always set global time when pushing to queue
+      
+      # assign "pair" (ti,ki) to every infected individual and add to queue 
+      if(infected != 0){
+        for(i in 1: infected){
+          if(runif(1) < pa){
+            ti <- distr_a(ta)
+            Q$push(c("a",times[t]+ti),-(ti+times[t]))
+            #print(c("added 1 a ", times[t], ti, times[t] + ti))
+          }else{
+            ti <- distr_b(tb)
+            Q$push(c("b",times[t]+ti),-(ti+times[t]))
+            #print(c("added 1 b ", times[t], ti,  times[t] + ti))
+          }
+        }
+      }
+    }
+    # gc() does it matter?
+  }
+  if(Q$size() != 0) print("queue not empty!") # handling?
+  M <- cbind(S,E,I,R,times) 
+}
+
--- estimatorsBothCases.R
+++ estimatorsBothCases.R
@@ -1,1 +1,166 @@
 # FOR ESTIMATION CODE
+
+# estimate R0 based on synthetic results
+# input: list of matrices
+# output: list of R0 estimates
+
+
+##################################
+######ESTIMATION - DISCRETE######
+########################################
+
+
+estimate_R0 <- function(result, cutoff=50,est_window=5){
+  n <- nrow(result[[1]])
+  S0 <- result[[1]][1,1]
+  R_0 <- rep(0,length(result))
+  for(i in 1:length(result)){
+    infected <- 0
+    infections <- 1
+    for(j in 1:n){
+      if(S0-result[[i]][j,1]>=8*sqrt(S0)) break # choice of const.?
+      if(S0-result[[i]][j,1]>=cutoff){
+        #x <- result[[i]][j,1]
+        #y <- result[[i]][(j+est_window),1]
+        #print("S0, i=epidemic nr, j=time, j+14, sus at j, sus at j+time")
+        #print(c(S0,i,j,j+14,x,y))
+        infected <- (result[[i]][j,1]-result[[i]][(j+est_window),1])
+        infections <- (result[[i]][(j+est_window+1),4]-result[[i]][j+1,4])
+        #print(c("time:",j,"infected:", infected, "infections/coughs:", infections))
+        R_0[i] <- R_0[i]+infected/infections
+        break
+      }
+    }
+  }
+  
+  return (R_0)
+}
+
+
+estimate_R0_growth <-  function(result, cutoff=50,est_window=8, T_G){
+  n <- nrow(result[[1]])
+  S0 <- result[[1]][1,1]
+  R_0 <- rep(0,length(result))
+  r <- 0
+  # for(i in 1:length(result)){
+  #    T_G <- T_G +min((1:nrow(result[[i]]))[result[[i]][,4]==max(result[[i]][,4])])
+  #  }
+  #  T_G <- T_G/n
+  
+  for(i in 1:length(result)){
+    #T_G <- 0
+    for(j in 1:n){
+      if(S0-result[[i]][j,1]>=cutoff){
+        #print(c(" 50 exceeded at time", j))
+        r <- log((S0-result[[i]][j+est_window,1])/(S0-result[[i]][j,1]))/est_window
+        #T_G <- T_G + result[[i]][n,4]/(min((1:n)[result[[i]][,4]==result[[i]][n,4]]))
+        R_0[i] <- R_0[i] + exp(r*T_G) # T_G how?
+        break
+      }
+    }
+  }
+  print(c("mean of estimates:", mean(R_0)))
+  return (R_0)
+}
+
+estimate_R0_final_size <- function(result){
+  ans <- rep(0,length(result))
+  S0 <- result[[1]][1,1]
+  for(i in 1:length(result)){
+    if(result[[i]][nrow(result[[i]]),1]/S0==1){ # deal with NaN
+      ans[i] <- -log(result[[i]][nrow(result[[i]]),1]/S0)/(1-(result[[i]][nrow(result[[i]]),1]-1)/S0)
+    }else{
+      ans[i] <- -log(result[[i]][nrow(result[[i]]),1]/S0)/(1-result[[i]][nrow(result[[i]]),1]/S0)
+    }
+  }
+  ans[ans<Inf] # unneeded?
+}
+
+compare_R0 <- function(result, cutoff=50,est_window=5,ta,ka,tb,kb,pa=0.5){
+  # calc analytic val (CHECK!):
+  R_0a <- ka*pa+kb*(1-pa)
+  estimate <- as.vector(estimate_R0(result,cutoff=cutoff, est_window = est_window))
+  estimate <- estimate[estimate>0]
+  print(c("mean of estimates", mean(estimate), "true value:", R_0a))
+  cbind(estimate, R_0a, estimate - R_0a)
+}
+
+##################################
+######ESTIMATION - CONTINUOUS######
+########################################
+
+
+# use Position() to find row of suitable time (def. as input)
+# use the row to check S_1 and S_2, calc. coughs?
+estimate_R0_V2 <- function(result, cutoff=50,est_time=5){
+  n <- nrow(result[[1]])
+  S0 <- result[[1]][1,1]
+  R_0 <- rep(0,length(result))
+  
+  for(i in 1:length(result)){
+    infected <- 0
+    infections <- 1
+    
+    for(j in 1:n){
+      if(S0-result[[i]][j,1]>=8*sqrt(S0)) break # choice of const.?
+      if(S0-result[[i]][j,1]>=cutoff){
+        est_time_rows <- Position(function(x) x > (result[[i]][j,5]+est_time), result[[i]][,5])
+        #x <- result[[i]][j,1]
+        #y <- result[[i]][(est_time_rows),1]
+        #print("S0, i=epidemic nr, j=starting row, end row, sus at j, sus at j+time")
+        #print(c(S0,i,j,est_time_rows,x,y))
+        infected <- (result[[i]][j,1]-result[[i]][est_time_rows,1])
+        infections <- est_time_rows-j # number of coughs
+        #print(c("time from:",j,"infected:", infected, "infections/coughs:", infections))
+        R_0[i] <- R_0[i]+infected/infections
+        break
+      }
+    }
+  }
+  
+  return (R_0)
+}
+
+
+# use Position() to find row of suitable time (def. as input)
+# use the row to check S_1 and S_2, def T_G as input
+
+estimate_R0_growth_V2 <-  function(result, cutoff=50,est_time=8, T_G){
+  n <- nrow(result[[1]])
+  S0 <- result[[1]][1,1]
+  R_0 <- rep(0,length(result))
+  r <- 0
+  
+  for(i in 1:length(result)){
+    #T_G <- 0
+    for(j in 1:n){
+      if(S0-result[[i]][j,1]>=cutoff){
+        #print(c(" 50 exceeded at time", j))
+        est_time_rows <- Position(function(x) x > (result[[i]][j,5]+est_time), result[[i]][,5])
+        r <- log((S0-result[[i]][est_time_rows,1])/(S0-result[[i]][j,1]))/est_time
+        #T_G <- T_G + result[[i]][n,4]/(min((1:n)[result[[i]][,4]==result[[i]][n,4]]))
+        R_0[i] <- R_0[i] + exp(r*T_G) # T_G how?
+        break
+      }
+    }
+  }
+  print(c("mean of estimates:", mean(R_0)))
+  print(c("var of estimates:", var(R_0)))
+  return (R_0)
+}
+
+
+# still works as previously - we only use final size 
+# NOTE: assumes whole population sus. at beginning! (see Diekmann et. al.)
+
+#estimate_R0_final_size <- function(result){
+#  ans <- rep(0,length(result))
+#  S0 <- result[[1]][1,1]
+#  for(i in 1:length(result)){
+#    if(result[[i]][nrow(result[[i]]),1]==1){
+#      result[[i]][nrow(result[[i]]),1] <- result[[i]][nrow(result[[i]]),1]+1
+#    }
+#    ans[i] <- -log(result[[i]][nrow(result[[i]]),1]/S0)/(1-result[[i]][nrow(result[[i]]),1]/S0)
+#  }
+#  ans[ans<Inf]
+#}
