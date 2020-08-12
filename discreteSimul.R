76a36b3ecbb7c19484196179ca45773c229d4292
mztiai <miikka.tiainen@aalto.fi>
2020-08-12 11:41:38 +0300

    7: discrete simul comments and documentation

--- discreteSimul.R
+++ discreteSimul.R
@@ -1,4 +1,15 @@
 # FOR DISCRETE SIMUL CODE
+#
+# simulation_binom_infectivity : n_events - number of rows = some upper bound on number of events
+#                                ta, ka - mean time and infectivity params for type a
+#                                tb, kb - mean time and infectivity params for type b
+#                                pa - probability of type a -> fraction of a in population
+#                                S0- number of initial susceptibles 
+#                                E0 - number of initial exposed individuals (future infectors at start)    
+# initialize first coughs based on initial state -> go forwards in time -> cough when events at that time
+# (i.e. either a[] or b[] vectors) -> infect based on type and update states -> keep going
+
+
 
 simulation_binom_infectivity <- function(time, ta,ka,tb,kb,pa=0.5, S0,E0,I0=0,R0=0){
   #set.seed(1234)
@@ -10,7 +21,7 @@ simulation_binom_infectivity <- function(time, ta,ka,tb,kb,pa=0.5, S0,E0,I0=0,R0
   a <- rep(0,time) # nr type a at time t
   b <- rep(0,time) # nr of type b at time t
   
-  # in discrete case events overlap
+  # NOTE: in discrete case events overlap
   
   total_population <- S0+E0+I0+R0
   
@@ -20,11 +31,9 @@ simulation_binom_infectivity <- function(time, ta,ka,tb,kb,pa=0.5, S0,E0,I0=0,R0
     if(p < pa){
       I[ta] <- I[ta] + 1
       a[ta] <- a[ta] + 1
-      #print("added 1 a")
     }else{
       I[tb] <- I[tb] + 1  
       b[tb] <- b[tb] + 1
-      #print("added 1 b")
     }
   }
   
@@ -44,21 +53,18 @@ simulation_binom_infectivity <- function(time, ta,ka,tb,kb,pa=0.5, S0,E0,I0=0,R0
       # infect based on previously drawn (ti,ki) pair status of infector
       if(a[t] != 0){
         a[t] <- a[t] - 1
-        #print("individual of type a - ka new infections")
         infectivity <- min(1,ka/total_population) 
         infected <- rbinom(1,S[t], infectivity)
       }else if(b[t] != 0){
         b[t] <- b[t] - 1
-        #print("individual of type b - kb new infections")
         infectivity <- min(1,kb/total_population)
         infected <- rbinom(1,S[t], infectivity)
       }
-      
-      #print(c("infect ", infected, " at time ", t))
+
       # individual has now infected ka/kb people - remove from E at t and recover at next time:
       S[t] <- S[t] - infected
       E[t] <- E[t] + infected - 1 # remove infector from exposed at current time, add caused infectees
-      R[t+1] <- R[t+1] + 1 # remove infectous from infected at next time
+      R[t+1] <- R[t+1] + 1        # remove infectous from infected at next time
       
       # assign pair (ti,ki) to every infected individual and assign values to future table entries
       # at appropriate times + add a/b to queue
@@ -67,15 +73,12 @@ simulation_binom_infectivity <- function(time, ta,ka,tb,kb,pa=0.5, S0,E0,I0=0,R0
           if(runif(1) < pa){
             I[t+ta] <- I[t+ta] + 1
             a[t+ta] <- a[t+ta] + 1
-            #print(c("added 1 a ", t))
           }else{
             I[t+tb] <- I[t+tb] + 1  
             b[t+tb] <- b[t+tb] + 1
-            #print(c("added 1 b ",t))
           }
         }
       }
-      
       if(R[t+1] == I[t]){break}
     }
   }
