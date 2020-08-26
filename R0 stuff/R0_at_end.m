eb13adbdccda6277f565540dcb71507e41702728
mztiai <miikka.tiainen@aalto.fi>
2020-08-26 10:23:18 +0300

    Changed folder path and added Matlab comments

--- R0 stuff/R0_at_end.m
+++
@@ -1,8 +0,0 @@
-function [R0_N_e] = R0_at_end(R0_H,kb,k,ka_end)
-%R0_AT_END Summary of this function goes here
-%   Detailed explanation goes here
-pa = (R0_H-kb)/(ka_end-kb);
-disp(pa);
-R0_N_e = calcR0_N(pa,ka_end,1-pa,kb,k);
-end
-
--- R0 stuff/calcR0_H.m
+++
@@ -1,6 +0,0 @@
-function [R0_H] = calcR0_H(pa,ka,pb,kb)
-%CALCR0_H Summary of this function goes here
-%   Detailed explanation goes here
-R0_H = pa*ka+pb*kb;
-end
-
